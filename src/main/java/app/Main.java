5a3e06f50b6825b8db9ce12b4b14c31e6c084fe1
Katsaros <katsarosgiannis1@gmail.com>
2024-08-05 03:56:59 +0300

    Simplify Main based on [1.1]

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,14 +1,10 @@
 package app;
 
-import app.endpoints.HelloEndpoint;
 import com.megadeploy.core.WebJavaServer;
-import com.megadeploy.endpoints.StatusEndpoint;
 
 public class Main {
     public static void main(String[] args) {
-        WebJavaServer server = new WebJavaServer(8080);
-        server.addEndpoint(new HelloEndpoint());
-        server.addEndpoint(new StatusEndpoint());
+        WebJavaServer server = new WebJavaServer(8080, Main.class);
 
         try {
             server.start();
